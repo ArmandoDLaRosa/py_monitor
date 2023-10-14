fb8f0c188681e940f2617181af122c2cb27aef9c
armando <A01570376@itesm.mx>
2023-10-13 23:22:05 -0600

    🐛 last_spikes

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from extensions import db
+from datetime import datetime
+import json
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.String(50), nullable=False)
+    timestamp = db.Column(db.String(50), nullable=False)  # DateTime
     description = db.Column(db.String(500), nullable=False)
     
 class SystemStat(db.Model):
@@ -12,3 +14,14 @@ class SystemStat(db.Model):
     memory_percentage = db.Column(db.Float, nullable=False)
     storage_percentage = db.Column(db.Float, nullable=False)
     
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super(DateTimeEncoder, self).default(obj)
+
+def get_last_5_spikes():
+    last_5_spikes = Event.query.order_by(Event.timestamp.desc()).limit(5).all()
+    return json.dumps([{'timestamp': spike.timestamp,
+                        'event': spike.description}
+                       for spike in last_5_spikes], cls=DateTimeEncoder)
