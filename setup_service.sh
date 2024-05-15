506ffc708c391635f4f549233e8393ef8fe8c881
ArmandoDLaRosa <a01570376@itesm.mx>
2024-05-15 02:51:47 +0100

    settings

--- setup_service.sh
+++ setup_service.sh
@@ -2,12 +2,32 @@
 
 # Define variables
 APP_NAME="py_monitor"
-APP_SCRIPT="/home/armando/Repos/py_monitor/app.py"
-APP_DIR="/home/armando/Repos/py_monitor/"
+APP_SCRIPT="/home/armando/Documents/py_monitor/app.py"
+APP_DIR="/home/armando/Documents/py_monitor/"
 USER="armando"
-VENV_DIR="/home/armando/Repos/myenv"
-CRON_FILE="/home/armando/Repos/py_monitor/crons/cron.txt" 
-BACKUP_CRON="/home/armando/Repos/py_monitor/crons/crontab_backup.txt" 
+VENV_DIR="/home/armando/Documents/py_monitor/myenv"
+CRON_FILE="/home/armando/Documents/py_monitor/crons/cron.txt" 
+BACKUP_CRON="/home/armando/Documents/py_monitor/crons/crontab_backup.txt" 
+
+# Check if MariaDB is installed, if not, install it
+if ! command -v mysql &> /dev/null
+then
+    echo "MariaDB is not installed. Installing MariaDB..."
+    sudo apt update
+    sudo apt install -y mariadb-server
+else
+    echo "MariaDB is already installed."
+fi
+
+# Check if Redis is installed, if not, install it
+if ! command -v redis-server &> /dev/null
+then
+    echo "Redis is not installed. Installing Redis..."
+    sudo apt update
+    sudo apt install -y redis-server
+else
+    echo "Redis is already installed."
+fi
 
 # Create systemd service file for your Flask app
 SERVICE_FILE="/etc/systemd/system/${APP_NAME}.service"
@@ -58,41 +78,21 @@ MYSQL_SCRIPT
 
 echo "MariaDB database and user have been created."
 
-INNODB_BUFFER_POOL_SIZE="1024M"  
-# This setting determines the size of the InnoDB buffer pool, which is an important memory area for caching data and indexes. 
-# A larger value can improve read performance but may require more memory.
-
-MAX_CONNECTIONS="50"             
-# This setting specifies the maximum number of concurrent connections that the MySQL/MariaDB server can handle.
-# It's important to set this value to a reasonable limit based on your server's resources and expected traffic.
-
-MEMORY_SIZE="2048M"  
-# This setting represents the amount of memory allocated to the entire MySQL/MariaDB process.
-# You should adjust this value based on your server's total memory and other resource requirements.
-
+# Add MariaDB configuration settings
+INNODB_BUFFER_POOL_SIZE="1024M"
+MAX_CONNECTIONS="50"
+MEMORY_SIZE="2048M"
 THREAD_STACK="512K"
-# This setting defines the stack size for each thread in the MySQL/MariaDB server.
-# Adjusting this value can affect the memory usage per thread. Smaller values may conserve memory.
-
 
 sudo sed -i "s/^\(innodb_buffer_pool_size\s*=\s*\).*\$/\1$INNODB_BUFFER_POOL_SIZE/" /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo sed -i "s/^\(max_connections\s*=\s*\).*\$/\1$MAX_CONNECTIONS/" /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo sed -i "s/^\(key_buffer_size\s*=\s*\).*\$/\1$MEMORY_SIZE/" /etc/mysql/mariadb.conf.d/50-server.cnf
 sudo sed -i "s/^\(thread_stack\s*=\s*\).*\$/\1$THREAD_STACK/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-
-CPU_CORES="0"  # Allow unlimited concurrency
-CORES="4" # I've 4 CPU cores
-
-sudo sed -i "s/^\(innodb_thread_concurrency\s*=\s*\).*\$/\1$CPU_CORES/" /etc/mysql/mariadb.conf.d/50-server.cnf
-sudo sed -i "s/^\(innodb_read_io_threads\s*=\s*\).*\$/\1$CORES/" /etc/mysql/mariadb.conf.d/50-server.cnf
-sudo sed -i "s/^\(innodb_write_io_threads\s*=\s*\).*\$/\1$CORES/" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-
 # Restart the MariaDB service to apply the changes
 sudo systemctl restart mysql.service
 
-echo "MariaDB conf file has been updated"
+echo "MariaDB conf file has been updated."
 
 # Backup current crontab
 crontab -l > "$BACKUP_CRON"
@@ -103,8 +103,7 @@ crontab "$CRON_FILE"
 echo "Current crontab has been backed up to '$BACKUP_CRON'."
 echo "Cron jobs have been updated to match '$CRON_FILE'."
 
+# Initialize Flask migrations
 ${VENV_DIR}/bin/python -m flask db init
 
-echo "Migrations folder existence was verified"
-
-
+echo "Migrations folder existence was verified."
